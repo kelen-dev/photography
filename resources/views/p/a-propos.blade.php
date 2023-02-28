def7232a5f6ff064a9eb8b635f5da4a2caeae139
kelen-dev <contact@kelens.fr>
2023-02-28 16:20:58 +0100

    Réfection du texte

--- resources/views/p/a-propos.blade.php
+++ resources/views/p/a-propos.blade.php
@@ -15,13 +15,20 @@
                     Je m'appelle SEGURA Sean.
                 </h5>
                 <p>
-                    Je suis un jeune développeur Web front-end et un concepteur UX / UI basé au Puy-en-Velay, en France.
+                    Je suis un jeune développeur Web principalement front-end et un concepteur UX / UI basé au
+                    Puy-en-Velay, en France.
                 </p>
                 <p>
                     J'aime vraiment résoudre des problèmes ainsi que rendre les choses jolies et faciles à utiliser. Je
-                    ne peux pas m'empêcher d'apprendre de nouvelles choses. Le plus, le mieux.
+                    ne peux pas m'empêcher d'apprendre de nouvelles choses. Le plus, le meilleur.
+                    <br>
                     J'aime aussi la photographie, un passe-temps que j'emmène depuis mon smartphone Realme 8 Pro. Oh, et
                     la danse. J'ai une passion pour la danse depuis mes 8 ans !
+                    <br>
+                    J'en fais depuis 11 ans dans l'école de danse La Clandestine et depuis septembre 2022 à Centre
+                    Danse. Là-bas, je fais du Street Dance, une danse que j'apprécie vraiment depuis que je l'ai
+                    découverte avec <a href="https://www.instagram.com/lana_boulaud/">Lana Boulaud</a> une jeune
+                    danseuse Parisienne connue via Instagram avec près de 5 000 followers.
                 </p>
             </article>
             <article id="travail">
@@ -31,27 +38,41 @@
                     </h2>
                 </div>
                 <h5>
-                    Je suis programmeur.
+                    Je suis développeur.
                 </h5>
                 <p>
-                    Pour le front-end, je travaille généralement avec HTML, de manière autonome. Je rends également le Web joli en utilisant Sass, CSS
-                    et, chaque fois que nécessaire, un de leurs amis : Bootstrap.
+                    Pour le front-end, je travaille généralement avec HTML, de manière autonome. Je rends également le
+                    Web joli en utilisant Sass, CSS et, chaque fois que nécessaire, un de leurs amis : Bootstrap.
                 </p>
                 <p>
-                    Pour le back-end, je travaille également avec Javascript (NodeJS). Mais, bien
-                    sûr, chaque fois que le projet nécessite PHP, je fais également du PHP avec le framework populaire, Laravel mais aussi WordPress.
+                    Au contraire du front-end, pour le back-end, je travaille avec des outils. Bien sûr, chaque fois que
+                    le projet nécessite PHP, j'en fais avec le framework populaire, Laravel mais aussi WordPress, si
+                    nécessaire pour le projet.
+                    <br>
+                    J'ai connu et commencé à utiliser Laravel, il y a quelques années, avec le CMS de dernière
+                    génération, Azuriom. Cliquez <a href="https://azuriom.com">ici</a> pour voir le site.
+                    <br>
+                    Azuriom est un CMS complet et pratique d'utilisation grâce au fait qu'il soit sous le framework
+                    Laravel mais aussi sous PHP 8.1. La dernière version de PHP en version release actuellement.
+                    <br>
+                    Ce CMS m'a fait apprendre beaucoup de choses par rapport à PHP. Mais aussi à l'utilisation du fameux
+                    Framework qu'il abrite et aux features qu'il embarque.
                 </p>
                 <h5>
                     Également designer.
                 </h5>
                 <p>
-                    Je me sens à l'aise avec de nombreux produits Adobe. Photoshop, Illustrator, ou Xd sont des
-                    standards de l'industrie et j'adore travailler avec eux.
+                    Je me sens à l'aise avec de nombreux logiciels de la suite Adobe. Photoshop, Illustrator, ou bien Xd
+                    sont des standards de l'industrie et j'adore travailler avec eux.
+                    <br>
+                    Interfaces utilisateur, design de sites / d'applications web... Vous m'appelez ! Comme je l'ai
+                    mentionné, créer de jolies choses est particulièrement important pour moi. J'aime quand tout est
+                    propre et bien organisé. Cela vient sûrement du fait que je sois Asperger.
+                    <br>
+                    Pour ceux qui ne le savent pas, le syndrome d'Asperger fait partie des TSA (troubles du spectre
+                    autistique).
                 </p>
                 <p>
-                    Interfaces utilisateur, design de sites / d'applications web... Vous m'appelez ! Comme je l'ai mentionné,
-                    créer de jolies choses est particulièrement important pour moi.
-                </p>
             </article>
         </section>
     </div>
