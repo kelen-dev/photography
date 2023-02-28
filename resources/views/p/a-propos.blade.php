462918897757c938a2390b944ad47cca0d66df95
kelen-dev <contact@kelens.fr>
2023-02-28 16:21:53 +0100

    Réfection du texte

--- resources/views/p/a-propos.blade.php
+++ resources/views/p/a-propos.blade.php
@@ -11,9 +11,11 @@
                         Qui suis-je ?
                     </h2>
                 </div>
+
                 <h5>
                     Je m'appelle SEGURA Sean.
                 </h5>
+
                 <p>
                     Je suis un jeune développeur Web principalement front-end et un concepteur UX / UI basé au
                     Puy-en-Velay, en France.
@@ -31,15 +33,19 @@
                     danseuse Parisienne connue via Instagram avec près de 5 000 followers.
                 </p>
             </article>
+
+
             <article id="travail">
                 <div class="title">
                     <h2 class="h3">
                         Qu'est-ce que je fais ?
                     </h2>
                 </div>
+
                 <h5>
                     Je suis développeur.
                 </h5>
+
                 <p>
                     Pour le front-end, je travaille généralement avec HTML, de manière autonome. Je rends également le
                     Web joli en utilisant Sass, CSS et, chaque fois que nécessaire, un de leurs amis : Bootstrap.
@@ -58,9 +64,11 @@
                     Ce CMS m'a fait apprendre beaucoup de choses par rapport à PHP. Mais aussi à l'utilisation du fameux
                     Framework qu'il abrite et aux features qu'il embarque.
                 </p>
+
                 <h5>
                     Également designer.
                 </h5>
+
                 <p>
                     Je me sens à l'aise avec de nombreux logiciels de la suite Adobe. Photoshop, Illustrator, ou bien Xd
                     sont des standards de l'industrie et j'adore travailler avec eux.
